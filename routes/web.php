546daba9f0f8bb50762207d34e232acf86ac55da
ASIF064 <asif064@gmail.com>
2023-12-23 19:27:19 +0600

    initial setup

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\TripController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('master');
+});
+
+Route::get('/trip', [TripController::class, 'addTrip'])->name('add.trip');
+Route::post('/trip', [TripController::class, 'storeTrip'])->name('store.trip');
+Route::get('/trip/search', [TripController::class, 'searchTrip'])->name('search.trip');
+
+
